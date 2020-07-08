f9e0ebef9d89da6a319060eb80ffcf4dfed871d3
SaraCremonesi <saracrem00@gmail.com>
2020-07-08 14:25:16 +0200

    Aggiunta commenti

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
   getList();
 
+  //
   $('#todo-add').click(function() {
+    // Leggo il valore della input
     var inputText = $('#todo-text').val();
 
+    // Se non è nullo, eseguo una chiamata ajax per appendere gli elementi alla lista (metodo POST),
+    // richiamando la funzione getList()
     if(inputText.length > 0) {
       $.ajax(
         {
@@ -27,26 +31,31 @@ $(document).ready(function() {
   }
 );
 
- $(document).on('click', '.delete',
- function() {
-   var id = $(this).parent().attr('data-id');
+// Evento di cancellazione di ogni singolo elemento al click su ciascun pulsante "Elimina"
+$(document).on('click', '.delete',
+function() {
+  // Richiamo l'id assegnato casualmente dal server a ciascun elemento creato
+  var id = $(this).parent().attr('data-id');
 
-   $.ajax(
-     {
-       url: 'http://157.230.17.132:3008/todos/' + id,
-       method: 'DELETE',
-       success: function(dataResponse) {
-         getList();
-         $('#todo-list').html('');
-       },
-       error: function() {
-         alert('Impossibile eliminare l\'elemento');
-       }
-     }
-   );
- });
+  // Eseguo la chiamata ajax per cancellare un elemento sia dalla pagina sia dal server (metodo DELETE)
+  $.ajax(
+    {
+      url: 'http://157.230.17.132:3008/todos/' + id,
+      method: 'DELETE',
+      success: function(dataResponse) {
+        getList();
+        $('#todo-list').html('');
+      },
+      error: function() {
+        alert('Impossibile eliminare l\'elemento');
+      }
+    }
+  );
+});
 
+// ****************FUNZIONI*******************
 
+// Creo una funzione con la chiamata ajax per ottenere la lista (metodo GET)
   function getList() {
     $.ajax(
       {
@@ -55,13 +64,18 @@ $(document).ready(function() {
         success: function(dataResponse) {
 
           if (dataResponse.length > 0) {
+
+            // Svuoto la input ad ogni click su "aggiungi"
             $('#todo-list').html('');
+
+            // Popolo la lista tramite Handlebars
             var source = $('#todo-template').html();
             var template = Handlebars.compile(source);
 
             for (var i = 0; i < dataResponse.length; i++) {
               var data = dataResponse[i];
               $('#todo-text').val('');
+
               var html = template(data)
               $('#todo-list').append(html);
             }
